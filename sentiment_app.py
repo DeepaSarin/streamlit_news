f2e0ec9400f09d96c2961a6ffca955af45a905cb
DeepaSarin <60520494+DeepaSarin@users.noreply.github.com>
2022-10-01 20:45:01 +0530

    Create sentiment_app.py

---
+++ sentiment_app.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+# import warnings
+# warnings.filterwarnings("ignore")
+# from PIL import Image
+
+pickle_in = open("news_sentiment.pkl","rb")
+classifier = pickle.load(pickle_in)
+pickle_vect = open("tfidf_vect.pkl","rb")
+tf_vect = pickle.load(pickle_vect)
+
+def predict_news_sentiment(News,classifier):
+    #Tfidf_vect = TfidfVectorizer(max_features=5000)
+    #Tfidf_vect.fit([News])
+    
+    News_tr=tf_vect.transform(News)
+    prediction=((classifier.predict(News_tr))[0])
+    print(prediction)
+    return prediction  
+    
+def Input_Output():
+    st.title("News Sentiment Analysis")
+    st.image("https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg", width=200)
+    
+    
+    st.markdown("A value for News Sentiment will be predicted using Random Forest Regressor (-1 being highly negative, +1 being highly positive and 0 a neutral News ",unsafe_allow_html=True)
+    st.markdown("Use Cases for Sentiment Analysis include :1.Customer Satisfaction Analysis 2.Analyse Customer Service Issues 3.Plan Product Improvements to name a few ",unsafe_allow_html=True)
+                
+    news  = [st.text_input("Enter News to be analyzed for Sentiment " , " ")]
+    
+    
+    
+    result = ""
+    if st.button("Click here to Predict"):
+        result = predict_news_sentiment(news,classifier)
+        st.balloons()
+    
+    
+    st.success('The Sentiment Predicted for this News Article is {}'.format(result))
+   
+if __name__ ==  '__main__':
+    Input_Output()
